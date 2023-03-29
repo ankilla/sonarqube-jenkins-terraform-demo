1dd1fb6a286f654c2cb5c1b7787d6c2292c302de
yourname <your email>
2023-03-29 22:14:31 +0530

    .

--- security-group.tf
+++ security-group.tf
@@ -1,8 +1,20 @@
 resource "aws_security_group" "allow_http_ssh" {
   name        = "allow-http-ssh"
   description = "Allow Ingress rules to allow SSH and HTTP connections"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 resource "aws_security_group" "allow_ssh_http" {
   name        = "allow_ssh_http"
   description = "Allow Ingress rules to allow HTTP and SSH connections"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
