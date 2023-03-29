1590af73218d21129bdbd706a53cccae9ad5ea57
yourname <your email>
2023-03-30 03:54:42 +0530

    added associate_public_ip_address = false

--- instance2.tf
+++ instance2.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "web1" {
+  associate_public_ip_address = false
   ami                         = var.image_id     #"ami-007855ac798b5175e"
   instance_type               = var.inst_type[1] // or var.inst_type[0], var.inst_type[2]
   count                       = var.inst_count
