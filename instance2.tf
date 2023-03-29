cc771b125161678c3cb43045e19bb31e57ff66f9
yourname <your email>
2023-03-30 03:31:27 +0530

    .

--- instance2.tf
+++ instance2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "web1" {
   ami                         = var.image_id     #"ami-007855ac798b5175e"
   instance_type               = var.inst_type[1] // or var.inst_type[0], var.inst_type[2]
   count                       = var.inst_count
