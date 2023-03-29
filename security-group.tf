86dda7d236083746ad7b70fe5edd527309855dd1
yourname <your email>
2023-03-29 17:47:05 +0530

    undo the changes for securitygroup

--- security-group.tf
+++ security-group.tf
@@ -2,7 +2,3 @@ resource "aws_security_group" "allow_http_ssh" {
   name        = "allow-http-ssh"
   description = "Allow Ingress rules to allow SSH and HTTP connections"
 }
-resource "aws_security_group" "allow_http_ssh1" {
-  name        = "allow-http-ssh1"
-  description = "Allow Ingress rules to allow SSH and HTTP connections"
-}
