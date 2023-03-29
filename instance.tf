00e95def5510d5cb0188a612253f6e59ef44c863
yourname <your email>
2023-03-29 23:38:47 +0530

    .

--- instance.tf
+++ instance.tf
@@ -1,27 +1,6 @@
-provider "aws" {
-  region = "us-east-1"
-}
 
-resource "aws_security_group" "example" {
-  name_prefix = "example"
-  description = "Allow SSH and HTTP traffic"
-  
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
-resource "aws_instance" "example" {
+/*resource "aws_instance" "example" {
   ami = "ami-007855ac798b5175e"
   instance_type = "t2.micro"
   security_groups = [aws_security_group.example.name]
@@ -29,4 +8,15 @@ resource "aws_instance" "example" {
   tags = {
     Name = "example-instance"
   }
+}*/
+
+resource "aws_instance" "web" {
+  ami                         = var.image_id     #"ami-007855ac798b5175e"
+  instance_type               = var.inst_type[1] // or var.inst_type[0], var.inst_type[2]
+  count                       = var.inst_count
+  availability_zone           = "us-east-1c"
+  key_name                    = "ec2demo"
+  vpc_security_group_ids      = [aws_security_group.example.name]
+
+  tags = var.ec2_instance_tags
 }
---
+++ security-group.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "example" {
+  name_prefix = "example"
+  description = "Allow SSH and HTTP traffic"
+  
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
