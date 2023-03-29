4f6d362122b9f6b656b013f1d63b5f7aef8fbffc
yourname <your email>
2023-03-29 23:24:40 +0530

    everything removed and created 1 file

--- instance.tf
+++ instance.tf
@@ -1,10 +1,32 @@
-resource "aws_instance" "web" {
-  ami                         = var.image_id     #"ami-007855ac798b5175e"
-  instance_type               = var.inst_type[1] // or var.inst_type[0], var.inst_type[2]
-  count                       = var.inst_count
-  availability_zone           = "us-east-1c"
-  key_name                    = "ec2demo"
-  #vpc_security_group_ids      = [allow_http_ssh,allow_ssh_http]
+provider "aws" {
+  region = "us-east-1"
+}
 
-  tags = var.ec2_instance_tags
+resource "aws_security_group" "example" {
+  name_prefix = "example"
+  description = "Allow SSH and HTTP traffic"
+  
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-007855ac798b5175e"
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.example.name]
+  
+  tags = {
+    Name = "example-instance"
+  }
 }
