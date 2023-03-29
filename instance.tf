91cbf44ae8aa1ab3b86ef7329041a60fc9b8305e
yourname <your email>
2023-03-30 02:42:00 +0530

    .

--- instance.tf
+++ instance.tf
@@ -31,12 +31,13 @@ provisioner "remote-exec" {
 
 ## Method 2 ##
 /*
-user_data = <<-EOF
-              #!/bin/bash
-              curl -L https://example.com/script.sh | bash  #download the script and execute using bash
-              EOF
-
-or
+resource "aws_instance" "web" {
+  ami                         = var.image_id     #"ami-007855ac798b5175e"
+  instance_type               = var.inst_type[1] // or var.inst_type[0], var.inst_type[2]
+  count                       = var.inst_count
+  availability_zone           = "us-east-1c"
+  key_name                    = "ec2demo"
+  vpc_security_group_ids      = [aws_security_group.allow_http_ssh.id]
 
 user_data = <<-EOF
                 #!/bin/bash
@@ -47,4 +48,7 @@ user_data = <<-EOF
                 echo "<h1>Nginx was deployed successful using terraform $(hostname -f)</h1>" > /usr/share/nginx/html/index.html
                 echo "<h1>Nginx was deployed successful using terraform $(hostname -f)</h1>" > /var/www/html/index.html
               EOF
+
+  tags = var.ec2_instance_tags
+}
 */
