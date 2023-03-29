7e29ccbe36ea8ece125d7982ccd5fc66ada32e74
yourname <your email>
2023-03-30 01:38:58 +0530

    restored everything as original

--- security-group.tf
+++ security-group.tf
@@ -1,25 +1,4 @@
 resource "aws_security_group" "allow_http_ssh" {
-  name_prefix = "allow_http_ssh"
-  description = "Allow SSH and HTTP traffic"
-  
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  name        = "allow-http-ssh"
+  description = "Allow Ingress rules to allow SSH and HTTP connections"
 }
