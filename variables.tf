f759fe5d7d008e03aafe989609f60fbb2d3da6b8
yourname <your email>
2023-03-29 22:09:10 +0530

    modified security groups

--- variables.tf
+++ variables.tf
@@ -29,5 +29,43 @@ variable "image_id" {
   }
 }
 
-variable "allow_http_ssh" {}
-variable "allow_ssh_http" {}
+variable "allow_http_ssh" {
+  ingress_ssh {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress_http {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress_allow_all {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+variable "allow_ssh_http" {
+  ingress_ssh {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress_http {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress_allow_all {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
