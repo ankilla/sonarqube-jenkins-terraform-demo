4a26d1ed39351a05e4856977ee1037439c2aaa0e
yourname <your email>
2023-03-30 02:43:35 +0530

    .

--- instance.tf
+++ instance.tf
@@ -7,7 +7,6 @@ resource "aws_instance" "web" {
   vpc_security_group_ids      = [aws_security_group.allow_http_ssh.id]
 
 
-## Method 1 ##
 connection {
     type     = "ssh"
     user     = "ubuntu"
@@ -15,6 +14,7 @@ connection {
     timeout  = "5m"
     host = self.public_ip
   }
+
 
 provisioner "remote-exec" {
     inline = [
@@ -28,27 +28,3 @@ provisioner "remote-exec" {
 }
   tags = var.ec2_instance_tags
 }
-
-## Method 2 ##
-/*
-resource "aws_instance" "web" {
-  ami                         = var.image_id     #"ami-007855ac798b5175e"
-  instance_type               = var.inst_type[1] // or var.inst_type[0], var.inst_type[2]
-  count                       = var.inst_count
-  availability_zone           = "us-east-1c"
-  key_name                    = "ec2demo"
-  vpc_security_group_ids      = [aws_security_group.allow_http_ssh.id]
-
-user_data = <<-EOF
-                #!/bin/bash
-                sudo apt update && apt -y dist-upgrade
-                sudo apt install -y nginx
-                sudo systemctl enable nginx
-                sudo systemctl start nginx
-                echo "<h1>Nginx was deployed successful using terraform $(hostname -f)</h1>" > /usr/share/nginx/html/index.html
-                echo "<h1>Nginx was deployed successful using terraform $(hostname -f)</h1>" > /var/www/html/index.html
-              EOF
-
-  tags = var.ec2_instance_tags
-}
-*/
