df18426076c59ba29cf593892117adf84ef4c8a3
yourname <your email>
2023-03-30 02:01:51 +0530

    .

--- instance.tf
+++ instance.tf
@@ -12,8 +12,8 @@ provisioner "remote-exec" {
         "sudo apt install -y nginx",
         "sudo systemctl enable nginx",
         "sudo systemctl start nginx",
-        "echo "<h1>Nginx was deployed successful using terraform $(hostname -f)</h1>" > /usr/share/nginx/html/index.html",
-        "echo "<h1>Nginx was deployed successful using terraform $(hostname -f)</h1>" > /var/www/html/index.html"
+        "echo '<h1>Nginx was deployed successful using terraform $(hostname -f)</h1>' > /usr/share/nginx/html/index.html",
+        "echo '<h1>Nginx was deployed successful using terraform $(hostname -f)</h1>' > /var/www/html/index.html"
     ]
 }
 
