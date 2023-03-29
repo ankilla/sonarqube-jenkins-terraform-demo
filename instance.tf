751db9d5dbdb7f6f6febed2bbcd5694c9c3ce701
yourname <your email>
2023-03-30 02:48:56 +0530

    .

--- instance.tf
+++ instance.tf
@@ -21,9 +21,9 @@ provisioner "remote-exec" {
         "sudo apt update && apt -y dist-upgrade",
         "sudo apt install -y nginx",
         "sudo systemctl enable nginx",
-        "sudo systemctl start nginx",
-        "echo '<h1>Nginx was deployed successful using terraform $(hostname -f)</h1>' > /usr/share/nginx/html/index.html",
-        "echo "<h1>Nginx was deployed successful using terraform $(hostname -f)</h1>' > /var/www/html/index.html"
+        "sudo systemctl start nginx"
+        #"echo '<h1>Nginx was deployed successful using terraform $(hostname -f)</h1>' > /usr/share/nginx/html/index.html",
+        #"echo "<h1>Nginx was deployed successful using terraform $(hostname -f)</h1>' > /var/www/html/index.html"
     ]
 }
   tags = var.ec2_instance_tags
